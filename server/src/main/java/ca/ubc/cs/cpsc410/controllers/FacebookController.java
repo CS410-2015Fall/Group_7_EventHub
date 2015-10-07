a18137983a300d286381c3a41c2577f2b2e5d5f8
Vincent Cheng <vcheng@debian.org>
2015-10-07 14:38:27 -0700

    add template to fetch facebook data

---
+++ server/src/main/java/ca/ubc/cs/cpsc410/controllers/FacebookController.java
@@ -0,0 +1,43 @@
+package cpsc410server.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class FacebookController {
+
+    private Facebook facebook;
+
+    @Autowired
+    public FacebookController(Facebook facebook) {
+        this.facebook = facebook;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getFeed(Model model) {
+        try {
+            if (!facebook.isAuthorized()) {
+                return "redirect:/connect/facebook";
+            }
+        } catch (NullPointerException npe) {
+            return "redirect:/connect/facebook";
+        }
+
+        /*model.addAttribute("facebookProfile",
+                facebook.userOperations().getUserProfile());
+        PagedList<Post> homeFeed = facebook.feedOperations().getHomeFeed();
+        model.addAttribute("feed", homeFeed);*/
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        model.addAttribute("friends", facebook.friendOperations().getFriendProfiles());
+        model.addAttribute("events", facebook.eventOperations().getAttending());
+
+        return "feed";
+    }
+
+}
