2a7eb6d566aee6a43a700ca77ae0fe69f38e3326
Vincent Cheng <vcheng@debian.org>
2015-10-07 14:28:04 -0700

    add starter project for server side code

---
+++ server/src/test/java/ca/ubc/cs/cpsc410/WesyncApplicationTests.java
@@ -0,0 +1,18 @@
+package ca.ubc.cs.cpsc410;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WesyncApplication.class)
+@WebAppConfiguration
+public class WesyncApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
