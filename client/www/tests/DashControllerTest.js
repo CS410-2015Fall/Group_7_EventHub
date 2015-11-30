b2cdcf1439ab95bf4ca966a5c5ff490e9edefd64
Napon Taratan <napon@napontaratan.com>
2015-11-29 21:26:44 -0800

    Added additional tests

--- client/www/tests/DashControllerTest.js
+++ client/www/tests/DashControllerTest.js
@@ -24,20 +24,106 @@ it('should have a valid controller scope', function() {
     expect($scope).toBeDefined();
 });
 
-// it('presentOption should call finalize event if user presses agree', function() {
-//     var mockTempEvent = {
-//         'name': 'testEvent',
-//         'description': 'testDescription',
-//         'location': 'testLocation',
-//         'startDate': new Date(10, 10, 10),
-//         'endDate': new Date(10, 10, 10),
-//         'duration': 60,
-//         'host': 'napon',
-//         'invitees': [],
-//         'confirmedInvitees': [] 
-//     };
-//     spyOn(ionicPopup, 'confirm').and.returnValue(true);
-//     $scope.presentOption(tempEvent);
-// });
+it('presentOption should show a popup', inject( function($q) {
+    var mockTempEvent = {
+        'name': 'testEvent',
+        'description': 'testDescription',
+        'location': 'testLocation',
+        'startDate': new Date(10, 10, 10),
+        'endDate': new Date(10, 10, 10),
+        'duration': 60,
+        'host': 'napon',
+        'invitees': [],
+        'confirmedInvitees': [] 
+    };
+
+    var returnedPromise = $q.defer();
+    spyOn(UserDataService, 'finalizeEvent');
+    spyOn(ionicPopup, 'confirm').and.returnValue(returnedPromise.promise);
+    $scope.presentOption(mockTempEvent);
+    expect(ionicPopup.confirm).toHaveBeenCalled();
+}));
+
+it('should load MapPicker with a valid modal', inject( function($q, $ionicModal) {
+    var mockLocation = "123.2, 321.2";
+
+    var returnedPromise = $q.defer();
+    returnedPromise.promise.then(function (value) {
+        expect(value).toBeDefined();
+        expect($scope.modal).toBeDefined();
+        expect($scope.modal.show).toHaveBeenCalled();
+    });
+    spyOn($ionicModal, 'fromTemplateUrl').and.returnValue(returnedPromise.promise);
+    $scope.loadMapPicker(mockLocation);
+    returnedPromise.resolve();
+}));
+
+it('should close map by removing modal', inject( function($q, $ionicModal) {
+    var mockLocation = "123.2, 321.2";
+
+    var returnedPromise = $q.defer();
+    returnedPromise.promise.then(function (value) {
+        $scope.closeModal();
+        expect($scope.modal).toBeDefined();
+        expect($scope.modal.remove).toHaveBeenCalled();        
+    });
+    spyOn($ionicModal, 'fromTemplateUrl').and.returnValue(returnedPromise.promise);
+    $scope.loadMapPicker(mockLocation);
+    returnedPromise.resolve();
+}));
+
+it('should call findTime and make API call with the correct path', function() {
+    spyOn(API, 'post');
+    $scope.findTime(13);
+    expect(API.post).toBeDefined();
+    expect(API.post).toHaveBeenCalledWith('event/findTime', jasmine.any(Object), jasmine.any(Function), jasmine.any(Function));
+});
+
+it('should call declineInvite by calling UserDataService with correct parameter', function() {
+    var mockTempEvent = {
+        'id': 10,
+        'name': 'testEvent',
+        'description': 'testDescription',
+        'location': 'testLocation',
+        'startDate': new Date(10, 10, 10),
+        'endDate': new Date(10, 10, 10),
+        'duration': 60,
+        'host': 'napon',
+        'invitees': [],
+        'confirmedInvitees': [] 
+    };
+
+    spyOn(UserDataService, 'declineInvite');
+    $scope.declineInvite(mockTempEvent);
+    expect(UserDataService.declineInvite).toBeDefined();
+    expect(UserDataService.declineInvite).toHaveBeenCalledWith(10);
+});
+
+it('should call acceptInvite by calling UserDataService with correct parameter', function() {
+    var mockTempEvent = {
+        'id': 10,
+        'name': 'testEvent',
+        'description': 'testDescription',
+        'location': 'testLocation',
+        'startDate': new Date(10, 10, 10),
+        'endDate': new Date(10, 10, 10),
+        'duration': 60,
+        'host': 'napon',
+        'invitees': [],
+        'confirmedInvitees': [] 
+    };
+
+    spyOn(UserDataService, 'acceptInvite');
+    $scope.acceptInvite(mockTempEvent);
+    expect(UserDataService.acceptInvite).toBeDefined();
+    expect(UserDataService.acceptInvite).toHaveBeenCalledWith(10);
+});
+
+it('should call refresh by calling UserDataService with correct parameter', function() {
+    spyOn(UserDataService, 'refresh');
+    $scope.refresh();
+    expect(UserDataService.refresh).toBeDefined();
+    expect(UserDataService.refresh).toHaveBeenCalled();
+});
 
 });
